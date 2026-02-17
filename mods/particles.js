eee2c97bd2fe640d5a312f320c608733244033ea
HTHThreee <hthnew@proton.me>
2026-02-17 20:54:22 +0200

    Added electron neutrino

--- mods/particles.js
+++ mods/particles.js
@@ -1,1 +1,11 @@
 // TODO: Add neutrinos, gluons, and W and Z and Higgs bosons.
+elements.electron_neutrino = {
+    color: "#26FF00",
+    behavior: behaviors.BOUNCY,
+    category: "energy",
+    desc: "The electron neutrino  is an elementary particle which has zero electric charge. It is the first neutrino.",
+    state: "gas",
+    maxSize: 1/*,
+    reactions: {
+    } */
+};
