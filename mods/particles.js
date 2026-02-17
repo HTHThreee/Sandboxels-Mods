7b5e1fa4862d349fe92670a7ce9d75444b0898b6
HTHThreee <hthnew@proton.me>
2026-02-17 21:57:59 +0200

    NOT DONE

--- mods/particles.js
+++ mods/particles.js
@@ -1,4 +1,31 @@
-// TODO: Add neutrinos, gluons, and W and Z and Higgs bosons.
+// TODO:
+// Add alpha, neutrinos, gluons, and W and Z and Higgs bosons.
+// Add human reactions.
+elements.electron = {
+    color: "#00AAAA",
+    behavior: behaviors.BOUNCY,
+    category: "energy",
+    description: "The alpha particle is a helium-4 nucleus.",
+    state: "gas",
+    charge: 1,
+    maxSize: 1
+    reactions: {
+        "positron": { elem1:"gamma_ray", elem2:"gamma_ray" }
+    }
+};
+
+elements.alpha_particle = {
+    color: "#FF00FF",
+    behavior: behaviors.BOUNCY,
+    category: "energy",
+    description: "The alpha particle is a helium-4 nucleus.",
+    state: "gas",
+    maxSize: 1
+    reactions: {
+        "electron
+    }
+};
+
 elements.electron_neutrino = {
     color: "#26FF00",
     behavior: behaviors.BOUNCY,
